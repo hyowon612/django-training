75c1fa3968399da13eb63cf5192f2bfba6f8cf1f
전효원 <hyowon612@gmail.com>
2021-08-02 23:08:27 +0900

    Create 구현

--- secondproject/blog/views.py
+++ secondproject/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
+from django.utils import timezone
 from .models import Blog
 # Create your views here.
 
@@ -9,3 +10,15 @@ def home(request):
 def detail(request, id):
     blog = get_object_or_404(Blog, pk = id)
     return render(request, 'detail.html', {'blog': blog})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.body = request.POST['body']
+    new_blog.pub_date = timezone.now()
+    new_blog.save()
+    return redirect('detail', new_blog.id)
