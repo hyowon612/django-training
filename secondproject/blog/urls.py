9ad6e770a7ff9dff6579fe0f55e87b655cf1aacc
전효원 <hyowon612@gmail.com>
2021-08-04 18:43:46 +0900

    Form

--- secondproject/blog/urls.py
+++ secondproject/blog/urls.py
@@ -6,8 +6,10 @@ app_name = 'blog'
 
 urlpatterns = [
     path('<int:id>', detail, name='detail'),
-    path('new/', new, name='new'),
-    path('create/', create, name='create'),
+    path('new_with_django_form',new_with_django_form, name='new_with_django_form'),
+    path('create_with_django_form', create_with_django_form, name='create_with_django_form'),
+    # path('new/', new, name='new'),
+    # path('create/', create, name='create'),
     path('update/<int:id>', update, name='update'),
     path('delete/<int:id>', delete, name='delete'),
 ]
--- secondproject/blog/views.py
+++ secondproject/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
 from .models import Blog
+from .forms import BlogForm
 # Create your views here.
 
 def home(request):
@@ -11,22 +12,35 @@ def detail(request, id):
     blog = get_object_or_404(Blog, pk = id)
     return render(request, 'detail.html', {'blog': blog})
 
-def new(request):
-    return render(request, 'new.html')
+def new_with_django_form(request):
+    form = BlogForm()
+    return render(request, 'new_with_django_form.html', {'form': form})
 
-def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.writer = request.POST['writer']
-    new_blog.body = request.POST['body']
-    new_blog.pub_date = timezone.now()
-    try:
-        new_blog.image = request.FILES['image']
-    except:
-        print()
-    # new_blog.image = request.FILES['image']
-    new_blog.save()
-    return redirect('blog:detail', new_blog.id)
+# def new(request):
+#     return render(request, 'new.html')
+
+def create_with_django_form(request):
+    form = BlogForm(request.POST, request.FILES)
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.pub_date = timezone.now()
+        new_blog.save()
+        return redirect('blog:detail', new_blog.id)
+    return redirect('home')
+
+# def create(request):
+#     new_blog = Blog()
+#     new_blog.title = request.POST['title']
+#     new_blog.writer = request.POST['writer']
+#     new_blog.body = request.POST['body']
+#     new_blog.pub_date = timezone.now()
+#     try:
+#         new_blog.image = request.FILES['image']
+#     except:
+#         print()
+#     # new_blog.image = request.FILES['image']
+#     new_blog.save()
+#     return redirect('blog:detail', new_blog.id)
 
 def update(request, id):
     blog = Blog.objects.get(id = id)
